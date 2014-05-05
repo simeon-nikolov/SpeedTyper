58751106d3d2b15f1792b499d050fdde6b0b1993
simeon-nikolov <simeon.v.nikolov@gmail.com>
2014-05-05 15:17:58 +0300

    added TextDao interface

---
+++ src/speedtyper/dao/TextDao.java
@@ -0,0 +1,13 @@
+package speedtyper.dao;
+
+import java.util.List;
+
+import speedtyper.model.Text;
+
+public interface TextDao {
+	public void add(Text text);
+	public void edit(Text text);
+	public void delete(int textId);
+	public Text getText(int textId);
+	public List<Text> getAllTexts();
+}
