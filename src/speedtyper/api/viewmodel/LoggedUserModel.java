daa24889375386d81acbe2fd16eb9799f8fef287
simeon-nikolov <simeon.v.nikolov@gmail.com>
2014-05-08 23:59:05 +0300

    Added LoggedUserModel for the response to the client

---
+++ src/speedtyper/api/viewmodel/LoggedUserModel.java
@@ -0,0 +1,22 @@
+package speedtyper.api.viewmodel;
+
+public class LoggedUserModel {
+	private String username;
+	private String sessionKey;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getSessionKey() {
+		return sessionKey;
+	}
+
+	public void setSessionKey(String sessionKey) {
+		this.sessionKey = sessionKey;
+	}
+}
