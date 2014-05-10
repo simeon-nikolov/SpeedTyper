dbd6a5a35a1f6b3482b84caad043eb121d413fb5
simeon-nikolov <simeon.v.nikolov@gmail.com>
2014-05-10 21:36:50 +0300

    Added RoomViewModel

---
+++ src/speedtyper/api/viewmodel/RoomViewModel.java
@@ -0,0 +1,53 @@
+package speedtyper.api.viewmodel;
+
+public class RoomViewModel {
+	private int id;
+	private String status;
+	private int participants;
+	private int maxParticipants;
+	private String text;
+
+	public RoomViewModel() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public int getParticipants() {
+		return participants;
+	}
+
+	public void setParticipants(int participants) {
+		this.participants = participants;
+	}
+
+	public int getMaxParticipants() {
+		return maxParticipants;
+	}
+
+	public void setMaxParticipants(int maxParticipants) {
+		this.maxParticipants = maxParticipants;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+}
