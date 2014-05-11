3a6752e0b9a5cf9352a2c17c43585aca87b7ed41
simeon-nikolov <simeon.v.nikolov@gmail.com>
2014-05-11 17:21:15 +0300

    Added getters and setters in JsonResponse class in order to serialize it to json

--- src/speedtyper/api/viewmodel/JsonResponse.java
+++ src/speedtyper/api/viewmodel/JsonResponse.java
@@ -9,4 +9,21 @@ public class JsonResponse {
 		this.status = status;
 		this.errorMessage = errorMessage;
 	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+	
 }
