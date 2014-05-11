9353bde069dca0f74b8758a398b720b91ce712b1
simeon-nikolov <simeon.v.nikolov@gmail.com>
2014-05-11 20:56:07 +0300

    update RoomController

--- src/speedtyper/api/controller/RoomController.java
+++ src/speedtyper/api/controller/RoomController.java
@@ -5,7 +5,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-import org.hibernate.Hibernate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
