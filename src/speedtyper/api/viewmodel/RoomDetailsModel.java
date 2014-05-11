a5045ea83d0f3845c20b6826277ad10540a96f3d
simeon-nikolov <simeon.v.nikolov@gmail.com>
2014-05-11 17:22:20 +0300

    Added model for room details for joining a room - RoomDetailsModel

---
+++ src/speedtyper/api/viewmodel/RoomDetailsModel.java
@@ -0,0 +1,73 @@
+package speedtyper.api.viewmodel;
+
+import java.util.List;
+
+public class RoomDetailsModel {
+	private int id;
+	private String status;
+	private String creator;
+	private List<String> participants;
+	private int participantsCount;
+	private int maxParticipants;
+	private String text;
+
+	public RoomDetailsModel() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getCreator() {
+		return creator;
+	}
+
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+
+	public List<String> getParticipants() {
+		return participants;
+	}
+
+	public void setParticipants(List<String> participants) {
+		this.participants = participants;
+	}
+	
+	public int getParticipantsCount() {
+		return participantsCount;
+	}
+
+	public void setParticipantsCount(int participantsCount) {
+		this.participantsCount = participantsCount;
+	}
+	
+	public int getMaxParticipants() {
+		return maxParticipants;
+	}
+
+	public void setMaxParticipants(int maxParticipants) {
+		this.maxParticipants = maxParticipants;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+}
