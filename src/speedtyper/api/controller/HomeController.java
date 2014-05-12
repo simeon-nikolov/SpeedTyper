bfa6b9ef80f9a198e521f35be6ffa06b2c363032
simeon-nikolov <simeon.v.nikolov@gmail.com>
2014-05-12 23:57:12 +0300

    trying to fix home route

--- src/speedtyper/api/controller/HomeController.java
+++ src/speedtyper/api/controller/HomeController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
-	@RequestMapping(value = "index", method = RequestMethod.GET)
+	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
-		return "index";
+		return "index.html";
 	}
 }
