6378fb6cdc9957d97ab6480b0269f7caabd62880
simeon-nikolov <simeon.v.nikolov@gmail.com>
2014-05-15 23:50:14 +0300

    updated HomeController in controllers.js - added menuLinks to

--- WebContent/WEB-INF/views/Scripts/app/controllers.js
+++ WebContent/WEB-INF/views/Scripts/app/controllers.js
@@ -1,5 +1,9 @@
 function HomeController($scope, $http) {
-	
+	$scope.menuLinks = [{addresss:"/", title:"Home", name:"Home"},
+	                    {addresss:"/rooms", title:"Create or join a typing race", name:"Play"},
+	                    {addresss:"/highscore", title:"Highscore", name:"Highscore"},
+	                    {addresss:"/user/view", title:"View profile", name:"Profile"},
+	                    {addresss:"/user/logout", title:"Logout", name:"Logout"}];
 }
 
 function LoginController($scope, $http) {
