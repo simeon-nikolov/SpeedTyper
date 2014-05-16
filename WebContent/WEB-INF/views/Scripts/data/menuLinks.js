b8d90f59d2779846e50cbbc8c0318363f04423cc
simeon-nikolov <simeon.v.nikolov@gmail.com>
2014-05-16 18:13:29 +0300

    added /Scripts/data to store json data

---
+++ WebContent/WEB-INF/views/Scripts/data/menuLinks.js
@@ -0,0 +1,21 @@
+[ {
+	"address" : "#/",
+	"title" : "Home",
+	"name" : "Home"
+}, {
+	"address" : "#/rooms",
+	"title" : "Create or join a typing race",
+	"name" : "Play"
+}, {
+	"address" : "#/highscore",
+	"title" : "Highscore",
+	"name" : "Highscore"
+}, {
+	"address" : "#/user/view",
+	"title" : "View profile",
+	"name" : "Profile"
+}, {
+	"address" : "#/user/logout",
+	"title" : "Logout",
+	"name" : "Logout"
+} ]
