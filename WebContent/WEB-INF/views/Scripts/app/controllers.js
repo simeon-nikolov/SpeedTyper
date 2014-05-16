14382a0aea4fb87b71e1a061b49890c68045ddc3
simeon-nikolov <simeon.v.nikolov@gmail.com>
2014-05-16 19:59:17 +0300

    Menu is extracted in separeted partial view and controller and included in index.html

--- WebContent/WEB-INF/views/Scripts/app/controllers.js
+++ WebContent/WEB-INF/views/Scripts/app/controllers.js
@@ -1,8 +1,13 @@
 function HomeController($scope, $http) {
+
+}
+
+function MenuController($scope, $http) {
 	$http.get("Scripts/data/menuLinks.js").success(function(links) {
 		$scope.menuLinks =  links;
 	});
 }
+
 
 function LoginController($scope, $http) {
 
