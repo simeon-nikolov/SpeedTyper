a9b4d575b287e45a0515c0589fe49e8b3e789e41
simeon-nikolov <simeon.v.nikolov@gmail.com>
2014-05-16 18:14:14 +0300

    update controllers.js

--- WebContent/WEB-INF/views/Scripts/app/controllers.js
+++ WebContent/WEB-INF/views/Scripts/app/controllers.js
@@ -1,19 +1,17 @@
 function HomeController($scope, $http) {
-	$scope.menuLinks = [{addresss:"/", title:"Home", name:"Home"},
-	                    {addresss:"/rooms", title:"Create or join a typing race", name:"Play"},
-	                    {addresss:"/highscore", title:"Highscore", name:"Highscore"},
-	                    {addresss:"/user/view", title:"View profile", name:"Profile"},
-	                    {addresss:"/user/logout", title:"Logout", name:"Logout"}];
+	$http.get("Scripts/data/menuLinks.js").success(function(links) {
+		$scope.menuLinks =  links;
+	});
 }
 
 function LoginController($scope, $http) {
-	
+
 }
 
 function RegisterController($scope, $http) {
-	
+
 }
 
 function RoomsController($scope, $http) {
-	
+
 }
